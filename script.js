917734ce0504a78140c96d6eda05791f571173f4
Adam Wankowski <peter.wanko@gmail.com>
2023-03-08 23:44:03 +0100

    more

--- script.js
+++ script.js
@@ -1,38 +1,84 @@
-const generateButton = document.querySelector('.generate-button');
-const copyButton = document.querySelector('.copy-button');
-const emojiOutput = document.querySelector('.emoji-output');
+// Select DOM elements
+const generateButtons = document.querySelectorAll('.generate-button');
+const copyButton = document.querySelector('#copy-button');
+const emojiOutput = document.querySelector('#emoji-output');
+const headline = document.querySelector('h1');
 
-// Generates a random number between min and max (inclusive)
+// Define the bear emoji character codes
+const bearEmojis = [
+  String.fromCodePoint(0x1F43B), // bear face
+  String.fromCodePoint(0x1F43C), // bear
+  String.fromCodePoint(0x1F9A5), // polar bear
+  String.fromCodePoint(0x1F98B), // grizzly bear
+  String.fromCodePoint(0x1F43A), // panda
+];
+
+// Generate a random integer between min (inclusive) and max (inclusive)
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Generates a string of bear emojis with a random length between 1 and 10
-function generateBearEmoji() {
-  const bearEmoji = '🐻';
-  const length = getRandomInt(1, 10);
-  return bearEmoji.repeat(length);
+// Generate a string of a random amount of bear emojis
+function generateBearEmoji(min, max) {
+  const numBearEmojis = getRandomInt(min, max);
+  let bearEmojiString = '';
+  for (let i = 0; i < numBearEmojis; i++) {
+    const randomIndex = getRandomInt(0, bearEmojis.length - 1);
+    bearEmojiString += bearEmojis[randomIndex];
+  }
+  return bearEmojiString;
 }
 
-// Copies the bear emoji string to the clipboard
+// Handles the "Generate Bear Emoji" button click
+function handleGenerateClick(event) {
+  const min = event.target.dataset.min;
+  const max = event.target.dataset.max;
+  const bearEmoji = generateBearEmoji(min, max);
+  emojiOutput.textContent = bearEmoji;
+}
+
+// Add event listeners to the "Generate Bear Emoji" buttons
+generateButtons.forEach((button) => {
+  button.addEventListener('click', handleGenerateClick);
+});
+
+// Add event listener to the "Copy Bear Emoji" button
+copyButton.addEventListener('click', copyBearEmoji);
+
+// Copies the current bear emoji string to the clipboard
 function copyBearEmoji() {
-  const emojiString = emojiOutput.textContent;
-  navigator.clipboard.writeText(emojiString)
-    .then(() => {
-      console.log('Emoji string copied to clipboard:', emojiString);
-      //alert('Bear emoji string copied to clipboard!');
-    })
-    .catch((err) => {
-      console.error('Failed to copy emoji string:', err);
-      //alert('Failed to copy bear emoji string :(');
-    });
+  const range = document.createRange();
+  range.selectNode(emojiOutput);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
 }
 
-generateButton.addEventListener('click', () => {
-  const bearEmojiString = generateBearEmoji();
-  emojiOutput.textContent = bearEmojiString;
+// Add event listener to the document to deselect the text on click outside
+document.addEventListener('click', () => {
+  window.getSelection().removeAllRanges();
 });
 
-copyButton.addEventListener('click', () => {
-  copyBearEmoji();
+// Add event listener to the headline to copy the bear emoji string
+headline.addEventListener('click', copyBearEmoji);
+
+// Move the emoji output over the buttons
+emojiOutput.style.order = '1';
+generateButtons.forEach((button) => {
+  button.style.order = '2';
 });
+
+// Add event listeners to the additional "Generate Bear Emoji" buttons
+const generateButton1 = document.querySelector('#generate-button-1');
+const generateButton2 = document.querySelector('#generate-button-2');
+const generateButton3 = document.querySelector('#generate-button-3');
+generateButton1.addEventListener('click', handleGenerateClick);
+generateButton2.addEventListener('click', handleGenerateClick);
+generateButton3.addEventListener('click', handleGenerateClick);
+generateButton1.dataset.min = '1';
+generateButton1.dataset.max = '5';
+generateButton2.dataset.min = '5';
+generateButton2.dataset.max = '20';
+generateButton3.dataset.min = '20';
+generateButton3.dataset.max = '100';
